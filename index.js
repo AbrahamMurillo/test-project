06cb4e6315801949b6bab39d6921ef323618a22d
Abraham Murillo <abraham.murillo04@gmail.com>
2021-04-05 18:27:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+const fn = function thuthlyArguments(a,b,c,d){
+    let truthyObject = []
+    const final = fn.toString()
+    
+    .replace(/((\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s))/mg,'')
+    .match(/^function\s*[^\(]*\(\s*([^\)]*)\)/m)[1]
+    .split(/,/)
+    for(let i=0;i<arguments.length;i++){
+        let pair=final[i]
+        if(arguments[i]){
+            console.log(pair)
+            var myObject = new Object();
+            myObject.argument = pair;
+            myObject.value = arguments[i]
+            truthyObject.push(myObject)
+        }
+    }
+    return truthyObject
+}
+
+//console.log(thuthlyArguments("Oscar",0,true,1));
+
+function reverseString(str) {
+    let splitString = str.split(""); 
+    let reverseArray = splitString.reverse(); 
+    let joinArray = reverseArray.join(""); 
+    return joinArray; 
+}
+
+//console.log(reverseString("Materialize"))
+
+app.get('/thuthly',(req,res)=>{
+    return res.json(fn("Oscar",0,true,'Mateo'))
+})
+
+app.get('/reverse/:str',(req,res)=>{
+    const {str} = req.params
+    return res.json(reverseString(str))
+})
+
+
+app.listen(3000,()=>console.log('Listening on port',3000))
